45045efe55317701611da094f5c0c3a841020fa5
otcz <54145021+otcz@users.noreply.github.com>
2021-08-21 10:41:31 -0500

    Primer commit
    Ejemplo

---
+++ codigo.py
@@ -0,0 +1,6 @@
+precio_dolar=3000
+numero =input("digita un numero")
+numero= float(numero)
+resultado= numero/precio_dolar
+resultado= round(resultado,2)
+print("Usted tiene $"+str(resultado)+", Mucha gracias")
